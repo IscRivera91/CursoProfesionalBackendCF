f58e6ebc4e2ed5e3854575bb103422981a1e2086
Ricardo Rivera Sanchez <ricardorivera.personal.91@gmail.com>
2021-06-26 11:08:28 -0500

    ...

--- MVC/server.js
+++ MVC/server.js
@@ -1,35 +1,88 @@
 const express = require('express');
-const mysql   = require('mysql');
+const sqlite3 = require('sqlite3');
+const bodyParser = require('body-parser');
+const Sequelize = require('sequelize');
+const methodOverride = require('method-override');
+const session = require('express-session');
+const socketio = require('socket.io');
+
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
-
-const connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : '',
-    database : 'curso-backend'
-});
+const tasksRoutes = require('./routes/tasks_routes');
+const registrationsRoutes = require('./routes/registrations_routes');
+const sessionsRoutes = require('./routes/sessions_routes');
+const categoriesRoutes = require('./routes/categories_routes');
 
 
-connection.connect();
+const findUserMiddleware = require('./middlewares/find_user');
+const authUser = require('./middlewares/auth_user');
 
-process.on('SIGINT',function(){
-    console.log('Bye ...');
-    connection.end();
-    process.exit();
-});
 
-function runQuery(query,params = {}){
-    connection.query(query,params, function (error, results, fields) {
-        if (error) throw error;
-        return results;
-    });
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.set('view engine','pug');
+
+let sessionConfig = {
+  secret:['12iasdhasjdasdhbasjd','123yhuasdgasdjkznxcjxzkh1y23'],
+  saveUninitialized: false,
+  resave: false
 }
 
-app.get('/', function(request, response){
-    response.send('Holis');
-});
+if(process.env.NODE_ENV && process.env.NODE_ENV == 'production'){
+  sessionConfig['store'] = new (require('connect-pg-simple')(session))();
+}
 
-app.listen(3000);
+app.use(session(sessionConfig));
+
+app.use(findUserMiddleware);
+app.use(authUser);
+
+app.use(tasksRoutes);
+app.use(registrationsRoutes);
+app.use(sessionsRoutes);
+app.use(categoriesRoutes);
+
+app.get('/',function(req,res){
+  res.render('home',{user: req.user});
+})
+
+
+let server = app.listen(process.env.PORT || 3000);
+let io = socketio(server);
+let sockets = {};
+
+
+let usersCount = 0;
+
+io.on('connection',function(socket){
+
+  let userId = socket.request._query.loggeduser;
+  if(userId) sockets[userId] = socket;
+  console.log(sockets);
+  // Actuaiza usuarios en tiempo real
+  usersCount++;
+  io.emit('count_updated',{count: usersCount});
+
+  socket.on('new_task',function(data){
+    if(data.userId){
+      let userSocket = sockets[data.userId];
+      if(!userSocket) return;
+
+      userSocket.emit('new_task',data);
+    }
+  })
+
+  socket.on('disconnect',function(){
+
+    Object.keys(sockets).forEach(userId=>{
+      let s = sockets[userId];
+      if(s.id == socket.id) sockets[userId] = null;
+    });
+
+    console.log(sockets);
+
+    usersCount--;
+    io.emit('count_updated',{count: usersCount});
+  })
+});
